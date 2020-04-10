008b6a7ca848ebec54233bf1d4f5e921ef030380
Alex <atm2g19@soton.ac.uk>
2020-04-10 21:17:34 +0100

    Created html-rendere added mobile-compatibility to html

--- src/sendFile.ts
+++ src/sendFile.ts
@@ -9,6 +9,31 @@ function staticServe(root: string) {
 }
 
 function sendFile(path: string, req: express.Request, res: express.Response, next: express.NextFunction) {
+	let type = path.substr(path.lastIndexOf('.'));
+	getFile(path)
+		.then(async file => {
+
+			let r = /#{.+}/gm;
+			let i;
+			if (type == '.html')
+				while (i = r.exec(file)) {
+					// console.log(i[0]);
+					try {
+						let include = await getFile(`public/include/${i[0].substring(2, i[0].length-1)}.html`);
+						file = file.substr(0, i.index) + include + file.substr(i.index + i[0].length);
+					} catch (e) {
+						file = file.substr(0, i.index) + file.substr(i.index + i[0].length);
+					}
+				}
+			res.type(type);
+			res.send(file);
+			res.end();
+		})
+		.catch(err => {
+			console.log(err);
+			next(404);
+		})
+	return;
 	fs.exists(path, (exists: boolean) => {
 		if (!exists) {
 			next(404);
@@ -32,6 +57,16 @@ function sendFile(path: string, req: express.Request, res: express.Response, nex
 
 		})
 	})
+}
+
+async function getFile(path: string): Promise < string > {
+	// console.log(`Getting file: ${path}`);
+	let handle = await fs.promises.open(path, 'r');
+	// console.log("HANDLE");
+	// console.log(handle);
+	let file = await handle.readFile();
+	handle.close();
+	return file.toString();
 }
 
 export {
